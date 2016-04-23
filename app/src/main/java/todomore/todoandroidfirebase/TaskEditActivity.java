c35874517f171515bf1fa145d036e2511752b768
Ian <ian@darwinsys.com>
2016-04-23 09:19:05 -0400

    Better variable name, reorder a few lines for readability

--- app/src/main/java/todomore/todoandroidfirebase/TaskEditActivity.java
+++ app/src/main/java/todomore/todoandroidfirebase/TaskEditActivity.java
@@ -27,11 +27,14 @@ public class TaskEditActivity extends AppCompatActivity {
 
         // If the user just deleted an item and that event hasn't yet made it to fhe main view,
         // they could crash the app by trying to edit it again. Block that, before setContentView().
-        int id = getIntent().getIntExtra(TaskDetailFragment.ARG_ITEM_INDEX, 0);
-        if (id < 0 || id >= ApplicationClass.sTasks.size()) {
+        int index = getIntent().getIntExtra(TaskDetailFragment.ARG_ITEM_INDEX, 0);
+        if (index < 0 || index >= ApplicationClass.sTasks.size()) {
             Toast.makeText(this, "Unable to edit that item", Toast.LENGTH_SHORT).show();
             finish();
         }
+        KeyValueHolder<String, Task> taskWrapper = ApplicationClass.sTasks.get(index);
+        mTask = taskWrapper.value;
+        mKey = taskWrapper.key;
 
         setContentView(R.layout.activity_task_edit);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -47,10 +50,6 @@ public class TaskEditActivity extends AppCompatActivity {
                 doSave();
             }
         });
-
-        KeyValueHolder<String, Task> taskWrapper = ApplicationClass.sTasks.get(id);
-        mTask = taskWrapper.value;
-        mKey = taskWrapper.key;
 
         modelToView();
     }
